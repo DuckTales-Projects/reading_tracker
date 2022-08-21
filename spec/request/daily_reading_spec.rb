b6b2e4b7b455eeecad621224006734f1309d6110
James Toledo <jamesmacena14@gmail.com>
2022-08-21 15:17:49 -0300

    Add daily reading negative case test
    Co-Authored-By: Jhonny Toledo <jhonnytoledo4975@gmail.com>

--- spec/request/daily_reading_spec.rb
+++ spec/request/daily_reading_spec.rb
@@ -17,5 +17,21 @@ describe 'DailyReading', type: :request do
         expect(DailyReading.count).to eq 1
       end
     end
+
+    context 'when receive a request more than once in a day' do
+      let(:create_daily_reading) { create(:daily_reading, reading_date: Time.zone.today) }
+      let(:params) { { daily_readings: { reading_date: Time.zone.today } } }
+
+      before do
+        create_daily_reading
+        post_to_daily_readings
+      end
+
+      it 'must ignore all other requests' do
+        expect(DailyReading.count).to eq 1
+        expect(response).to have_http_status :ok
+        expect(response.body).to eq 'Read successfully confirmed!'
+      end
+    end
   end
 end
