2474e348d4c0b44a115f163b654676de34ec4175
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-08-22 21:23:03 -0300

    Update daily_reading_spec.rb

--- spec/request/daily_reading_spec.rb
+++ spec/request/daily_reading_spec.rb
@@ -4,11 +4,9 @@ require 'rails_helper'
 
 describe 'DailyReading', type: :request do
   describe 'POST /daily_readings' do
-    subject(:post_to_daily_readings) { post daily_readings_path, params: params }
+    subject(:post_to_daily_readings) { post daily_readings_path }
 
     context 'when the request is successful' do
-      let(:params) { { daily_readings: { reading_date: Time.zone.today } } }
-
       before { post_to_daily_readings }
 
       it 'returns a successful response' do
@@ -19,8 +17,7 @@ describe 'DailyReading', type: :request do
     end
 
     context 'when receive a request more than once in a day' do
-      let(:create_daily_reading) { create(:daily_reading, reading_date: Time.zone.today) }
-      let(:params) { { daily_readings: { reading_date: Time.zone.today } } }
+      let(:create_daily_reading) { create(:daily_reading, reading_date: Date.current) }
 
       before do
         create_daily_reading
@@ -31,6 +28,31 @@ describe 'DailyReading', type: :request do
         expect(DailyReading.count).to eq 1
         expect(response).to have_http_status :ok
         expect(response.body).to eq 'Read successfully confirmed!'
+      end
+    end
+
+    context 'when the system receives a requisition with the incorrect date' do
+      subject(:post_to_daily_readings) { post daily_readings_path, params: params }
+
+      let(:create_daily_reading) { create(:daily_reading, reading_date: 2.days.ago.to_date) }
+      let(:params) { { daily_readings: { reading_date: Date.yesterday } } }
+
+      before do
+        create_daily_reading
+        post daily_readings_path
+        post_to_daily_readings
+      end
+
+      it 'only the reading referring to the correct date must be confirmed' do
+        old_daily_reading = DailyReading.first.reading_date
+        last_daily_reading = DailyReading.last.reading_date.to_date
+
+        expect(response).to have_http_status :ok
+        expect(response.body).to eq 'Read successfully confirmed!'
+        expect(DailyReading.count).to eq 2
+        expect(old_daily_reading).to eq 2.days.ago.to_date
+        expect(last_daily_reading).to eq Date.current
+        expect(last_daily_reading).not_to eq Date.yesterday
       end
     end
   end
