9129b53814006724b17bc3a1a615fe40820d2ad1
Marcelo Toledo <marcelotoledo5000@gmail.com>
2022-08-23 18:17:04 -0300

    Fix I18n

--- app/controllers/daily_readings_controller.rb
+++ app/controllers/daily_readings_controller.rb
@@ -2,10 +2,10 @@
 
 class DailyReadingsController < ApplicationController
   def create
-    return render status: :ok, body: t('read_confirmed') if DailyReading.last&.reading_date == Date.current
+    return render status: :ok, body: I18n.t('read_confirmed') if DailyReading.last&.reading_date == Date.current
 
     DailyReading.create!(reading_date: Date.current)
 
-    render status: :created, body: t('read_confirmed')
+    render status: :created, body: I18n.t('read_confirmed')
   end
 end
