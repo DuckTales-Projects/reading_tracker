e72ab6e0296ac2cf29519677aab00cbce49c8f2d
James Toledo <jamesmacena14@gmail.com>
2022-08-26 16:26:16 -0300

    Update daily_readings_controller.rb
    Co-Authored-By: Jhonny Toledo <jhonnytoledo4975@gmail.com>

--- app/controllers/daily_readings_controller.rb
+++ app/controllers/daily_readings_controller.rb
@@ -4,8 +4,14 @@ class DailyReadingsController < ApplicationController
   def create
     return render status: :ok, body: I18n.t('read_confirmed') if DailyReading.last&.reading_date == Date.current
 
-    DailyReading.create!(reading_date: Date.current)
+    DailyReading.create!(daily_readings_params)
 
     render status: :created, body: I18n.t('read_confirmed')
+  end
+
+  private
+
+  def daily_readings_params
+    params.require(:daily_readings).permit(:user_id).merge(reading_date: Date.current)
   end
 end
