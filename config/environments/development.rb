cfff3e16eff002e43968512f90594d5209d8df8c
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-09-01 10:25:20 -0300

    setup devise-jwt
    CI
    Setup devise-jwt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: ENV.fetch('DB_HOST'), port: ENV.fetch('DB_PORT') }
   config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
